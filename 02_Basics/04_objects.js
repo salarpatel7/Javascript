9304c2fc1e6a08be2b2a712b5c4d607491163c46
salar patel <70138079+salarpatel7@users.noreply.github.com>
2023-08-18 14:38:11 +0000

    objects day 2

--- 02_Basics/04_objects.js
+++ 02_Basics/04_objects.js
@@ -33,5 +33,15 @@ const tinderUser={}
     4:"d"
  }
 
- const obj3= Object.assign({}, objOne,objeTwo)   //
- console.log(obj3);
+ //const obj3= Object.assign({}, objOne,objeTwo)   //
+ //
+
+ const obj3 = {...objOne,...objeTwo}
+ //console.log(obj3);
+
+//  console.log(Object.keys(tinderUser));
+//  console.log(Object.values(tinderUser));
+//  console.log(Object.entries(tinderUser));
+//if u wanna know ex name property is there or not in object 
+
+console.log(tinderUser.hasOwnProperty('sa')) //
