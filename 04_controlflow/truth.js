e5b1796289efc948a9ea90764bedf6b52fddb39e
salar patel <70138079+salarpatel7@users.noreply.github.com>
2023-08-23 15:55:11 +0000

    truthy and falsy

---
+++ 04_controlflow/truth.js
@@ -0,0 +1,37 @@
+// const userEmail = "sp@ai.com" truth
+const userEmail = ""   //false
+
+
+//in this we havent given == or != but its assuming it is truth
+// if(userEmail){
+//     console.log("got users mail");
+// }else{
+//     console.log("dont have mail");
+// }
+
+/*
+falsy values
+false,0,-0,Bigint 0n,null,undefined,NaN
+
+truthy values
+ture
+"0"
+'false'
+" "   --one space in ""
+[]  , {}
+function(){}
+*/
+
+// const user = []
+// if(userEmail.length===0){
+//     console.log('array is empty');
+// }
+
+
+// const emptyObj={}
+// if(Object.keys(emptyObj).length === 0){
+//     console.log("empty");
+// }
+
+//nullish coaliecing operator (??)
+ 
