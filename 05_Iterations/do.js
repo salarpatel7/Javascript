5741f8e12a17e61cd72b0359bd0153093ae30a49
salar patel <70138079+salarpatel7@users.noreply.github.com>
2023-08-27 07:52:06 +0000

    while and do while

---
+++ 05_Iterations/do.js
@@ -0,0 +1,21 @@
+let index= 0
+
+// while (index <= 10) {
+//     console.log(`index value is ${index}`);
+//     index = index + 2
+// }
+
+let myArr=['superman','batman','shaktiman']
+
+let arr=0
+// while (arr < myArr.length) {
+//     console.log(`value is ${myArr[arr]}`)
+//     arr++
+// }
+
+
+let score=1
+do {
+    console.log(`score is ${score}`);
+    score++
+} while (score <= 10);
