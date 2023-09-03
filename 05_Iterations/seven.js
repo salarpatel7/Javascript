4380ee5df6017f055f85a339d0421dc7b3c64e38
salar patel <70138079+salarpatel7@users.noreply.github.com>
2023-09-03 14:58:03 +0000

    basic practice

--- 05_Iterations/seven.js
+++ 05_Iterations/seven.js
@@ -9,8 +9,9 @@ const myNumbers = [1,2,3,4,5,6,7,8]
  const newlist = myNumbers.map((num)=> num * 10 )
                             .map( (num) => num + 1)
                             .filter( (num) => num > 30)
+                            .map((num) => num + 5)
 
-// console.log(newlist);
+ console.log(newlist);
 
 
 
