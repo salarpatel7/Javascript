81dd6612d0cc968662bb33f184a2ce3cd8755010
salar patel <70138079+salarpatel7@users.noreply.github.com>
2023-10-16 07:42:48 +0000

    new and this keyword revise

---
+++ 10_objects_and_oop/classes2.js
@@ -0,0 +1,6 @@
+//classes 
+//methods 
+//property
+
+const nums = [1,2,3]
+nums.push()
--- 10_objects_and_oop/oop.js
+++ 10_objects_and_oop/oop.js
@@ -1,15 +1,15 @@
 //object litterals === nothing but object
 
-const user ={
-    username:"salar",
-    loginCount:8, 
-    signedIn:true,
+// const user ={
+//     username:"salar",
+//     loginCount:8, 
+//     signedIn:true,
 
-    getUserInfo:function(){
-        // console.log("got user details ");
-        console.log(`username: ${this.username}`);
-    }
-}
+//     getUserInfo:function(){
+//         // console.log("got user details ");
+//         console.log(`username: ${this.username}`);
+//     }
+// }
 
 // console.log(user.getUserInfo());
 
@@ -18,15 +18,45 @@ const user ={
 // const promiseOne= new Promise()
 // const date = new Date()
 
-function User(username,logincount,isloggedin){
-    this.username=username            //here (this) is variable storing value in this var we can take any name 
-    this.count=logincount
-    this.isloggedin=isloggedin
+//function User(username,logincount,isloggedin){
+ //   this.username=username            //here (this) is variable storing value in this var we can take any name 
+    // this.count=logincount
+    // this.isloggedin=isloggedin
 
     // return this
+//}
+
+// const userOne= new User("salar",12,true)
+// const usertwo= new User("palli",9,false)
+// console.log(userOne);
+// console.log(usertwo);
+
+
+
+const user= {
+    username: "hitesh",
+    loginCount:8,
+    signedIn:true,
+
+    getUserDetails:function(){
+        // console.log("got user details ");
+        // console.log(`username:${this.username}`);
+        console.log(this);
+    }
 }
 
-const userOne= new User("salar",12,true)
-const usertwo= new User("palli",9,false)
-console.log(userOne);
-console.log(usertwo);
+// console.log(user.username);
+// console.log(user.getUserDetails());
+
+function User(username,countlogin,isloggedin){
+    this.username=username
+    this.countlog=countlogin
+    this.isloggedin=isloggedin
+
+    // return this   // we can write or not it dosnt matter 
+}
+
+const info = new User("saalaar" , 12, false )
+const prop =new  User("cake" , 20, true )
+console.log(info);
+console.log(prop);
