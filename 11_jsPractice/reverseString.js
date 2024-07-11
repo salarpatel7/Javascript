478e805542b2c4fd379d0f1e195d51df23fb0ebb
salar patel <70138079+salarpatel7@users.noreply.github.com>
2024-07-11 05:53:55 +0000

    reverse a string in js

---
+++ 11_jsPractice/reverseString.js
@@ -0,0 +1,9 @@
+function reversString(str){
+    // var strApp=str.split("")
+    // var reverseIt=strApp.reverse()
+    // var joinIt=reverseIt.join()
+
+    return str.split("").reverse().join("")
+}
+
+console.log(reversString("hello"));
