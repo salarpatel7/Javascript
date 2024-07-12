8ae0c0c9887b57604a30023a99dac5bda0e9774e
salar patel <70138079+salarpatel7@users.noreply.github.com>
2024-07-12 07:13:34 +0000

    updates

--- 11_jsPractice/filterStudents.js
+++ 11_jsPractice/filterStudents.js
@@ -17,16 +17,36 @@ const STUDENTS=[
     
 ]
 
-function filterAge(STUDENTS ,age){
-    const newArr=[]
-    for (let studnet of STUDENTS){
-        if(studnet.age>=age){
-        newArr.push(studnet)
+// function filterAge(STUDENTS ,age){
+//     const newArr=[]
+//     for (let studnet of STUDENTS){
+//         if(studnet.age>=age){
+//         newArr.push(studnet)
 
-        }
-    }
+//         }
+//     }
 
-    return newArr
-}
+//     return newArr
+// }
 
-console.log(filterAge(STUDENTS,45));
+// console.log(filterAge(STUDENTS,45));
+
+// console.log(
+    
+// STUDENTS.filter(
+//     (student)=>{
+//         return student.age>=20
+//     }
+// )
+// )
+
+const people = [
+    { name: "Alice", age: 30, department: "Engineering" },
+    { name: "Bob", age: 25, department: "Marketing" },
+    { name: "Charlie", age: 35, department: "Engineering" },
+  ];
+  newarry=[]
+ const filtersEng=people.filter((engineers)=>engineers.department==="Engineering")
+  
+  console.log(filtersEng);
+  // Output: [{ name: "Alice", age: 30, department: "Engineering" }, { name: "Charlie", age: 35, department: "Engineering" }]
